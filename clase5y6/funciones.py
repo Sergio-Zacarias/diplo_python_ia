b0d8451e0ecccf06a2968e057ad1370dd2172161
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2026-02-21 02:17:09 -0300

    Funciones

---
+++ clase5y6/funciones.py
@@ -0,0 +1,72 @@
+# 1- Desarrollar un programa con dos funciones. La primer solicite el ingreso de un entero y
+# muestre el cuadrado de dicho valor. La segunda que solicite la carga de dos valores y
+# muestre el producto de los mismos. LLamar desde el bloque del programa principal a ambas
+# funciones
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 2- Desarrollar un programa que solicite la carga de tres valores y muestre el menor. Desde el
+# bloque principal del programa llamar 2 veces a dicha función (sin utilizar una estructura
+# repetitiva)
+
+
+
+
+
+# ------------------------------------------------------------------------------------------- 
+# 3-  Desarrollar una funcion que reciba un string como parametro y nos muestre la cantidad de
+# vocales. Llamarla desde el bloque principal del programa 3 veces con string distintos
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 4- Confeccionar una función que reciba tres enteros y los muestre ordenados de menor a
+# mayor. En otra función solicitar la carga de 3 enteros por teclado y proceder a llamar a la
+# primer función definida
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 5-  Elaborar una función que reciba tres enteros y nos retorne el valor promedio de los mismos
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 6- Elaborar una función que nos retorne el perímetro de un cuadrado pasando como parámetros
+# el valor de un lado
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 7- Confeccionar una función que calcule la superficie de un rectángulo y la retorne, la función
+# recibe como parámetros los valores de dos de sus lados:
+# def retornar_superficie(lado1,lado2):
+# En el bloque principal del programa cargar los lados de dos rectángulos y luego mostrar cual
+# de los dos tiene una superficie mayor
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 8- Plantear una función que reciba un string en mayúsculas o minúsculas y retorne la cantidad
+# de letras 'a' o 'A
+
+
+
+
+
+# -------------------------------------------------------------------------------------------
+# 9- Confeccionar una función que reciba entre 2 y 5 enteros. La misma nos debe retornar la
+# suma de dichos valores. Debe tener tres parámetros por defecto
