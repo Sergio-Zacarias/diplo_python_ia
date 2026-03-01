d678ce06bb6b349e1c6b1d15c81810f459929b96
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2026-03-01 01:06:24 -0300

    Practicando listas

---
+++ clase7y8/listas.py
@@ -0,0 +1,45 @@
+# 1- Definir por asignación una lista con 8 elementos enteros.
+# Contar cuantos de dichos valores almacenan un valor superior a 100
+# Con While
+# lista = [101,2,300,4,5,670,100,830]
+# sup_cien = 0
+# x = 0
+# while x < len(lista):
+#     if lista[x] > 100:
+#         sup_cien += 1
+#     x+=1
+# print(f"La cantidad de valores superiores a cien es: {sup_cien}")
+# Con For
+# lista = [101,2,300,4,5,670,100,830]
+# sup_cien = 0
+# for x in range(len(lista)):
+#     if lista[x] > 100:
+#         sup_cien+=1
+# print(f"La cantidad de valores superiores a cien es: {sup_cien}")
+
+# -------------------------------------------------------------------------------------------
+# 2- Definir una lista por asignación con 5 enteros. Mostrar por pantalla solo
+# los elementos con valor iguales o superiores a 7
+# lista = [10,2,33,7,5]
+
+# for x in range(len(lista)):
+#     if lista[x] >= 7:
+#         print(lista[x])
+# lista = [10,2,33,7,5]
+# x = 0
+# while x < len(lista):
+#     if lista[x] >= 7:
+#         print(lista[x])
+#     x+=1
+
+# -------------------------------------------------------------------------------------------
+# 3- Definir una lista que almacene por asignación los nombres de 5 personas.
+# Contar cuantos de esos nombres tienen 5 o más caracteres
+# lista = ["Sergio", "Gladis", "Juan", "Leonardo", "Facundo"]
+# mayores = 0
+# for x in range(len(lista)):
+#     if len(lista[x]) >= 5:
+#         mayores += 1
+# print(f"La cantidad de nombres con 5 o mas caracteres es: {mayores}")
+
+# -------------------------------------------------------------------------------------------
